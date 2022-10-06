599824ffc24096bd5d44dfb8c9d6525d309670b5
Seongjae <cdaosldk@naver.com>
2022-10-06 14:52:29 +0900

    22/10/06

--- crolling.py
+++ crolling.py
@@ -5,9 +5,11 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.84m7k3d.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
 
+# URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.naver?sel=pnt&date=20210829',headers=headers)
 
+# HTML을 BeautifulSoup이라는 라이브러리를 활용해 검색하기 용이한 상태로 만듦
 soup = BeautifulSoup(data.text, 'html.parser')
 
 title = soup.select_one('#old_content > table > tbody > tr:nth-child(2) > td.title > div > a')
@@ -19,6 +21,7 @@ print(title['href'])
 
 #old_content > table > tbody > tr:nth-child(2) > td:nth-child(1) > img
 
+# select를 이용해서, tr들을 불러오기
 movies = soup.select('#old_content > table > tbody > tr')
 
 for movie in movies:
